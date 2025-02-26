7fb7339c303112ac8f4060c8af0039825ed976c7
Mohit Saini <mohitsainig436@gmail.com>
2025-02-26 20:31:40 +0530

    vite.config updated

--- new-client-css/vite.config.js
+++ new-client-css/vite.config.js
@@ -6,4 +6,11 @@ export default defineConfig({
   css: {
     modules: false, // Ensures global CSS files are not treated as modules
   },
+  define: {
+    'process.env': process.env,
+  },
+  preview: {
+    port: 4173, // Default Vite preview port, can be omitted
+    allowedHosts: ['job-portal-2-67c4.onrender.com', 'localhost'], // Allow Render host and localhost
+  },
 });
